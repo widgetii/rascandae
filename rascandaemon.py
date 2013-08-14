8a7c0e0be16e26e1175a575e9734bfae6b4301ab
Lyubimov Denis <lyubimov.denis@gmail.com>
2013-08-14 09:13:24 +0400

    Modified behaviour when image file already exists. gphoto2 now overwrites without hanging up

--- rascandaemon.py
+++ rascandaemon.py
@@ -33,8 +33,11 @@ def capture_into_filename(filename):
     '''
 
     logger.debug("Trying to save image to %s", filename)
-    subprocess.check_output(['sudo', 'gphoto2', '--capture-image-and-download', '--filename', filename]);
 
+    if os.path.exist(filename):
+        logger.warning("File %s already exists it will be overwritten", filename)
+        
+    subprocess.check_output(['sudo', 'gphoto2', ' -- force-overwrite --capture-image-and-download', '--filename', filename])
 
 def check_request():
 
